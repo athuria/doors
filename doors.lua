d963323bb4bd870759cd90b224f4c41c818ea163
athuria <110605779+athuria@users.noreply.github.com>
2022-10-14 22:48:55 +0700

    Update doors.lua

--- doors.lua
+++ doors.lua
@@ -15,6 +15,8 @@ local VisualsTab = Window:MakeTab({
 local CF = CFrame.new
 local LatestRoom = game:GetService("ReplicatedStorage").GameData.LatestRoom
 local ChaseStart = game:GetService("ReplicatedStorage").GameData.ChaseStart
+Bind:Set(Enum.KeyCode.RightCtrl)
+
 
 local KeyChams = {}
 VisualsTab:AddToggle({
